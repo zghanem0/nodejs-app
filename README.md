a38ef2ef281b3fa1b02d9aa6b99e0de8540cd083
root <root@192.168.1.50>
2020-03-15 06:42:35 +0200

    first-commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Clone the repo
+
+```docker build -t test1 . ```
+
+```docker run -d  -p 7777:7777 test1```
+
+
+
---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+// Constants
+const PORT = 7777;
+const HOST = '0.0.0.0';  //local host
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('welcome to ghanem :) \n');
+});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
